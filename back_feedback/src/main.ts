51d1cb5bbee09094ee4e541c8c8b92b2b8e6faeb
lucy.park <lucy.park@aloys.co.kr>
2025-07-01 15:46:53 +0900

    feat) 입력값 검증 추가

--- back_feedback/src/main.ts
+++ back_feedback/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as cookieParser from "cookie-parser";
+import { ValidationPipe } from "@nestjs/common";
 
 (BigInt.prototype as any).toJSON = function () {
   return this.toString();
@@ -8,6 +9,13 @@ import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   app.enableCors({
     origin: "http://localhost:3002",
     credentials: true,
