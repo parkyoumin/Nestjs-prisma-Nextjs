6ee84e4ac2727aec811e32facf119a22a507bfc0
lucy.park <lucy.park@aloys.co.kr>
2025-07-02 14:02:45 +0900

    로그인 데이터 오류

--- front_feedback/src/services/userService.ts
+++ front_feedback/src/services/userService.ts
@@ -3,7 +3,7 @@ import { get } from "@/api";
 const getUser = async () => {
   try {
     const res = await get("/user");
-    return res;
+    return res.data;
   } catch (error) {
     console.error("Error fetching user:", error);
     throw error;
